fef1f992e0e11815bfd01bd5cd21c7d5bf7bb99e
Nekodigi <uekaz@outlook.jp>
2024-02-24 09:10:46 +0900

    utility update

---
+++ essential/setup.sh
@@ -0,0 +1,22 @@
+echo '
+GPU_URL=""
+USER_NAME=""
+USER_PASS=""
+MS_USER_NAME=""
+MS_USER_PASS=""
+PROXY_URL=""
+PORT=""
+
+proxy=$MS_USER_NAME:$MS_USER_PASS@$PROXY_URL:$PORT
+http_proxy="http://$proxy"
+https_proxy="$http_proxy"
+ftp_proxy="$http_proxy"
+socks_proxy="$http_proxy"
+HTTP_PROXY="$http_proxy"
+HTTPS_PROXY="$http_proxy"
+
+export http_proxy https_proxy ftp_proxy socks_proxy HTTP_PROXY HTTPS_PROXY' > ~/proxy.sh
+
+echo 'source ~/proxy.sh' >> ~/.bash
+echo 'source ~/proxy.sh' >> ~/.bash_profile
+echo 'source ~/proxy.sh' >> ~/.zshrc
---
+++ essential/utils.sh
@@ -0,0 +1,12 @@
+sync() {
+    #do things with parameters like $1 such as
+    git add .
+    git commit -m '${1}'
+    git push origin HEAD
+}
+
+syncPull() {
+    #do things with parameters like $1 such as
+    git reset --hard origin/HEAD
+    git pull
+}
