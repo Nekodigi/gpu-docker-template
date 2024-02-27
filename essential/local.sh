3aaebfa42f184a816ac0e73e4e30c51759f0a59a
Nekodigi <uekaz@outlook.jp>
2024-02-27 15:14:09 +0900

    consistent name

--- essential/local.sh
+++ essential/local.sh
@@ -1,14 +1,14 @@
-USER =""
 GPU_URL=""
+USER_NAME =""
 
 yes y |  ssh-keygen -t rsa -b 4096   
-scp ~/.ssh/id_rsa.pub $USER@$GPU_URL:~/.ssh/authorized_keys
+scp ~/.ssh/id_rsa.pub $USER_NAME@$GPU_URL:~/.ssh/authorized_keys
 
 cd ~
 git clone https://github.com/Nekodigi/gpu-docker-template.git
 cd gpu-docker-template/essential
 
-scp setup.sh $USER@$GPU_URL:~/setup.sh
-scp test.sh $USER@$GPU_URL:~/test.sh
-ssh $USER@$GPU_URL sh setup.sh
-ssh $USER@$GPU_URL "source ~/.zshrc; sh test.sh"
+scp setup.sh $USER_NAME@$GPU_URL:~/setup.sh
+scp test.sh $USER_NAME@$GPU_URL:~/test.sh
+ssh $USER_NAME@$GPU_URL sh setup.sh
+ssh $USER_NAME@$GPU_URL "source ~/.zshrc; sh test.sh"
