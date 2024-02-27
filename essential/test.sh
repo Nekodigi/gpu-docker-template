1548e2e76fd9121343de30959c5f282a9a1f5ad5
Nekodigi <uekaz@outlook.jp>
2024-02-27 16:11:19 +0900

    fix test sh

--- essential/test.sh
+++ essential/test.sh
@@ -4,21 +4,21 @@ if [ $? -ne 0 ] ; then
 fi
 echo "Proxy OK"
 
-if ! command -v docker &> /dev/null
+if ! [ -x "$(command -v docker)" ]; then
 then
     echo "dockerがありません。管理者にインストールを申請してください"
     exit 1
 fi
 echo "docker OK"
 
-if ! command -v nvidia-ctk &> /dev/null
+if ! [ -x "$(command -v nvidia-ctk)" ]; then
 then
     echo "nvidia-ctk(NVIDIAコンテナツールキット)がありません。管理者にインストールを申請してください"
     exit 1
 fi
 echo "nvidia-ctk OK"
 
-if ! command -v nvidia-smi &> /dev/null
+if ! [ -x "$(command -v nvidia-smi)" ]; then
 then
     echo "nvidia-smi(NVIDIAドライバ)がありません。管理者にインストールを申請してください"
     exit 1
