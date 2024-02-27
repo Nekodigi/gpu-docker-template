36db206122ed0994504273909453ed3dc54c1a72
Nekodigi <uekaz@outlook.jp>
2024-02-27 15:12:38 +0900

    source rc

--- essential/local.sh
+++ essential/local.sh
@@ -11,4 +11,4 @@ cd gpu-docker-template/essential
 scp setup.sh $USER@$GPU_URL:~/setup.sh
 scp test.sh $USER@$GPU_URL:~/test.sh
 ssh $USER@$GPU_URL sh setup.sh
-ssh $USER@$GPU_URL sh test.sh
+ssh $USER@$GPU_URL "source ~/.zshrc; sh test.sh"
