acb411f4b8316f825f04581d3a7aad69eb6d03a5
Nekodigi <uekaz@outlook.jp>
2024-02-27 16:06:29 +0900

    better test

--- essential/test.sh
+++ essential/test.sh
@@ -1,5 +1,26 @@
 wget -q --spider google.com 
 if [ $? -ne 0 ] ; then
-  echo "ERROR"
+  echo "Proxyの設定が間違っています"
 fi
-echo "OK"
+echo "Proxy OK"
+
+if ! command -v docker &> /dev/null
+then
+    echo "dockerがありません。管理者にインストールを申請してください"
+    exit 1
+fi
+echo "docker OK"
+
+if ! command -v nvidia-ctk &> /dev/null
+then
+    echo "nvidia-ctk(NVIDIAコンテナツールキット)がありません。管理者にインストールを申請してください"
+    exit 1
+fi
+echo "nvidia-ctk OK"
+
+if ! command -v nvidia-smi &> /dev/null
+then
+    echo "nvidia-smi(NVIDIAドライバ)がありません。管理者にインストールを申請してください"
+    exit 1
+fi
+echo "nvidia-smi OK"
